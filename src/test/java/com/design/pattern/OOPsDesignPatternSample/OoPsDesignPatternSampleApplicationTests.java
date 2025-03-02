9dcda50e6409abb539a937bf77b51c306fffc2e9
Jitender.Yadav <jy2054050@gmail.com>
2025-03-02 11:50:32 +0530

    Initial setup code commit

---
+++ src/test/java/com/design/pattern/OOPsDesignPatternSample/OoPsDesignPatternSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.pattern.OOPsDesignPatternSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OoPsDesignPatternSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
